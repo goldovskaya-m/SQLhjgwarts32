67d9bafec9cbcbfac1867b446ebf0d457ad8aa2c
goldovskaya-m <ariya-aaa@yandex.ru>
2025-01-01 18:09:05 -0800

    Initial commit

---
+++ src/main/java/pro/skypro/SQL32/Sql32Application.java
@@ -0,0 +1,13 @@
+package pro.skypro.SQL32;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sql32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sql32Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/skypro/SQL32/Sql32ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.skypro.SQL32;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sql32ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
